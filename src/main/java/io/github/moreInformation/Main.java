66cbbc6871b89e64907739ffa01eb8f0cd67accd
Mcrtin <66840021+Mcrtin@users.noreply.github.com>
2022-07-07 13:09:51 +0200

    Initial commit

---
+++ src/main/java/io/github/moreInformation/Main.java
@@ -0,0 +1,8 @@
+package io.github.moreInformation;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MOD_ID)
+public class Main {
+    public static final String MOD_ID = "moreInformation";
+}
